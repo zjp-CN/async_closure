e43879d579ce969e75fed6c9b003cc601e8ca670
zjp <jiping_zhou@foxmail.com>
2023-03-11 13:33:28 +0800

    add(example::urlo): case3

--- examples/urlo.rs
+++ examples/urlo.rs
@@ -58,8 +58,55 @@ mod case2 {
     }
 }
 
+// src: https://users.rust-lang.org/t/async-constrain-lifetime-of-fut-type-param-to-lifetime-of-for-func-f-fn-fut/90676
+mod case3 {
+    use async_closure::{async_closure_once as cb, capture_lifetimes::AsyncFnOnce};
+
+    pub async fn test() -> Result<()> {
+        generic_fn(cb!({
+            ctx: Ctx<'a> = Ctx(&1)
+        }; async | | -> Result<u32> {
+            specific_fun_1(&ctx).await
+        }))
+        .await?;
+
+        generic_fn(cb!({
+            ctx: Ctx<'a> = Ctx(&2)
+        }; async | | -> Result<u64> {
+            specific_fun_2(&ctx).await
+        }))
+        .await?;
+
+        Ok(())
+    }
+
+    async fn generic_fn<'env, T, F>(f: F) -> Result<()>
+    where
+        F: AsyncFnOnce<'env, (), Output = Result<T>>,
+        T: std::fmt::Display,
+    {
+        let t = f.call_once(()).await?;
+        println!("{t}");
+
+        Ok(())
+    }
+
+    async fn specific_fun_1(ctx: &Ctx<'_>) -> Result<u32> {
+        Ok(u32::from(*ctx.0))
+    }
+
+    async fn specific_fun_2(ctx: &Ctx<'_>) -> Result<u64> {
+        Ok(u64::from(*ctx.0))
+    }
+
+    struct Ctx<'ctx>(&'ctx u16);
+    type AppErr = Box<dyn std::error::Error>;
+    type Result<T> = std::result::Result<T, AppErr>;
+}
+
 #[pollster::main]
 async fn main() {
     case1::test().await;
     case2::test().await;
+    case3::test().await.unwrap();
 }
