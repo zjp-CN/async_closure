32311471d463357c84d9f5309fa982e657fc6cac
Cor Peters <c.peters@eekels.com>
2024-07-04 11:04:02 +0200

    async_closures now compiles on stable without using features.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
-#![feature(async_fn_in_trait)]
-#![allow(incomplete_features)]
+// #![feature(async_fn_in_trait)]
+// #![allow(incomplete_features)]
 #![forbid(unsafe_code)]
 
 pub mod capture_lifetimes;
